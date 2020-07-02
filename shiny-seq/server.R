c03ddd58a94728bf7e500eaddb3a638568ba1bd5
Todd Farmer <wtoddfarmer@gmail.com>
2020-07-02 14:05:51 -0400

    updated Readmes and Shiny-seq

--- shiny-seq/server.R
+++ shiny-seq/server.R
@@ -1,13 +1,33 @@
 library(shiny)
 library(ggplot2)
 library(Seurat)
-
+source("/Volumes/one/DATA/drop-seq/DropSeuratFunctions.R")
 # load data
-SO <- readRDS("GP_downsample.RDS")
+# load all of the data
+# load all tissues
+
+folder <- "/Volumes/one/DATA/drop-seq/"
+SOs <- list.files(folder, pattern='*\\_downSample.RDS', recursive=TRUE, full.names=TRUE)
+loadDownsampleData(SOs)
+ALL_calc <- readRDS("../ALL/ALL_calc_downSample.RDS")
+
+tissue_list = list("CB", "ENT", "FC", "GP", "HC", "PC", "SN", "STR", "TH")
+class_list = list("ASTROCYTE",
+                  "ENDOTHELIAL_STALK",
+                  "ENDOTHELIAL_TIP",
+                  "EPENDYMAL",
+                  "MACROPHAGE",
+                  "MICROGLIA",
+                  "MURAL",
+                  "NEUROGENESIS",
+                  "NEURON",
+                  "OLIGODENDROCYTE",
+                  "POLYDENDROCYTE")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    # Image of cell classes on UMAP
+  
+     # Image of cell classes on UMAP
     output$classUmap <- renderImage({
         filename = "class.png"
         list(src = filename,
@@ -16,18 +36,73 @@ shinyServer(function(input, output) {
              height = 300,
              alt = "UMAP projection showing classes")
     }, deleteFile = FALSE)
-
-    # UMAP
-    output$umapPlot <- renderPlot({
-       FeaturePlot(SO, features = input$GOI) 
+    
+    # Level 1 Variable
+    output$L1 <- renderText({paste0("Level 1: ", input$l1)})
+    
+    # UMAP with Class
+    # add if statement for 'none'
+    output$classPlot <- renderPlot({
+        DimPlot(ALL_calc, group.by = "class", pt.size = 0.25, label = TRUE, repel =TRUE)  + theme(legend.position = "none")
     }, width = 600, height = 400)
     
-    # Violin
-    output$vlnPlot <- renderPlot({
-        VlnPlot(SO, features = input$GOI, group.by = "class", ) + ggtitle(NULL) + theme(legend.position = "none")
+    
+    # UMAP with Expressiom
+    output$umapPlot <- renderPlot({
+       FeaturePlot(ALL_calc, features = input$GOI) 
+    }, width = 600, height = 400)
+    
+    # Violin Class
+    output$vlnClass <- renderPlot({
+        VlnPlot(ALL_calc, features = input$GOI, group.by = "class" ) + ggtitle(NULL) + theme(legend.position = "none")
     }, width = 600, height = 400) 
     
+    # Violin Tissue
+    output$vlnTissue <- renderPlot({
+        VlnPlot(ALL_calc, features = input$GOI, group.by = "tissue" ) + ggtitle(NULL) + theme(legend.position = "none")
+    }, width = 600, height = 400) 
     
-        
-          
+    # Level 1 UMAP plot
+    # add conditionals for plots
+    # if L2 == none then "common_name' on L1 umap
+    output$L1overview <- renderPlot({
+        if (input$l1 %in% tissue_list){
+                DimPlot(get(input$l1), group.by = "class", pt.size = 0.25, label = TRUE, repel =TRUE)  + ggtitle(NULL) + theme(legend.position = "none")}
+        else  if (input$l1 %in% class_list){
+                DimPlot(get(input$l1), group.by = "tissue", pt.size = 0.25, label = TRUE, repel =TRUE)  + ggtitle(NULL) + theme(legend.position = "none")}    
+    }, width = 600, height = 400)     
+    
+    
+    output$umapL1 <- renderPlot({
+        #if (){}
+        FeaturePlot(get(input$l1), features = input$GOI)  + ggtitle(NULL) + theme(legend.position = "none")
+    }, width = 600, height = 400)
+   
+    
+    #Level 2
+    output$L2 <- renderText({paste0("Level 2: ", input$l2)})
+    
+    
+    output$report <- downloadHandler(
+        # For PDF output, change this to "report.pdf"
+        filename = "report.html",
+        content = function(file) {
+            # Copy the report file to a temporary directory before processing it, in
+            # case we don't have write permissions to the current working dir (which
+            # can happen when deployed).
+            tempReport <- file.path(tempdir(), "report.Rmd")
+            file.copy("report.Rmd", tempReport, overwrite = TRUE)
+            
+            # Set up parameters to pass to Rmd document
+            params <- list(n = input$slider)
+            
+            # Knit the document, passing in the `params` list, and eval it in a
+            # child of the global environment (this isolates the code in the document
+            # from the code in this app).
+            rmarkdown::render(tempReport, output_file = file,
+                              params = params,
+                              envir = new.env(parent = globalenv())
+            )
+        })
+    outputOptions(output, 'L1', suspendWhenHidden = FALSE)
 })
--- shiny-seq/ui.R
+++ shiny-seq/ui.R
@@ -1,41 +1,81 @@
 
 library(shiny)
 
+tissue_list = list("CB", "ENT", "FC", "GP", "HC", "PC", "SN", "STR", "TH")
+class_list = list("ASTROCYTE",
+                  "ENDOTHELIAL_STALK",
+                  "ENDOTHELIAL_TIP",
+                  "EPENDYMAL",
+                  "MACROPHAGE",
+                  "MICROGLIA",
+                  "MURAL",
+                  "NEUROGENESIS",
+                  "NEURON",
+                  "OLIGODENDROCYTE",
+                  "POLYDENDROCYTE")
+
 shinyUI(fluidPage(
 
     # Application title
     titlePanel("Shiny-seq: Single Cell Gene Expression Report"),
-    tags$h4("This app shows the expression of a single gene in the mouse globus palidus. The data used is from the publication", 
+    tags$h4("This app shows the expression of a single gene. The data used is from the publication", 
             tags$a(href="https://www.cell.com/cell/fulltext/S0092-8674(18)30955-3?", "Saunders et. al, 2018"),
             "and was processed using the R Package",
             tags$a(href="https://satijalab.org/seurat/", "Seurat.")),
-    tags$h2("Plots shown:"),
-    tags$h4("UMAP: the level of expression on the dimensional reduction plot."),
-    tags$h4("Violin Plot: the level of expression in each cell class."),
+    
     # Sidebar with gene entry field
     sidebarLayout(
         sidebarPanel(
             tags$h1("Gene of Interest"),
-            textInput("GOI", "Input a mouse gene symbol without quotes (i.e. Slc1a3) to see which cell types express it :", value = "Slc1a3", width = NULL,
+            textInput("GOI", "Input a mouse gene symbol without quotes (i.e. Slc1a3) to see which cell types express it :", value = "none", width = NULL,
                       placeholder = NULL),
-            tags$h4("Some Ideas:"),
-            tags$h6("Slc1a2 is expressed in astrocytes"),
-            tags$h6("Slc17a6 is expressed in some excitory neurons"),
-            tags$h6("Gad1 is expressed in some inhibitory neurons"),
-            tags$h6("Olig2 is expressed in oligodendrocytes"),
-            tags$h6("Tek is expressed in endotheilal cells"),
-            tags$h1("Cell Classes on UMAP"),
-            imageOutput("classUmap")
-              
+            selectInput("l1", "Select Level 1:",
+                        list('none',
+                            `Tissue` = tissue_list,
+                             `Class` = class_list)
+            ),
+            selectInput("l2", "Select Level 2:",
+                        list('none',
+                            `Tissue` = tissue_list,
+                             `Class` = class_list)
+            ),
+           # imageOutput("classUmap"),
+            
+            #downloadButton("report", "Generate report")
+            
         ),
 
         mainPanel(
-            # Show a plots
-            tags$h1("Expression of GOI on UMAP"),
+            tabsetPanel(type = "tabs",
+                        tabPanel("Level 0", 
+                                 tags$h1("Level 0: All Cells/Regions/Classes"),
+                                 tags$h2("Cell Class on UMAP"),
+                                 plotOutput("classPlot"),
+                                 tags$h2("Expression of GOI on UMAP"),
+                                 plotOutput("umapPlot"),
+                                 tags$h2("Violin Plot GOI in Cell Classes"),
+                                 plotOutput("vlnClass"),
+                                 tags$h2("Violin Plot GOI by Tissue"),
+                                 plotOutput("vlnTissue"),
+                                 ),
+                        tabPanel("Level 1", 
+                                 textOutput("L1"),
+                                 tags$head(tags$style("#L1{font-size: 36px; font-weight: 500; line-height: 1.1}")),
+                                 plotOutput("L1overview"),
+                                 tags$h2("Expression of GOI on L1 UMAP"),
+                                 plotOutput("umapL1")),
+                        tabPanel("Level 2", 
+                                 textOutput("L2"),
+                                 tags$head(tags$style("#L2{font-size: 36px; font-weight: 500; line-height: 1.1}")),
+                                 #plotOutput("L2overview"),
+                                 tags$h2("Expression of GOI on L2 UMAP"),
+                                 #plotOutput("umapL1")),
+                                 )
+            )
             
-            plotOutput("umapPlot"),
-            tags$h1("Violin Plot GOI in Cell Classes"),
-            plotOutput("vlnPlot")
+        
+            
+            #includeHTML("../ALL/ALL_calc.html")
         )
     )
 ))
